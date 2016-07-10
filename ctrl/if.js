185b2ad9bdd95cacdafbd4b8cc275a074e453984
Yuya Tsuneoka <tsuneoka@mizuuchi.lab.tuat.ac.jp>
2016-07-10 22:48:30 +0900

    if, switch文をやってみた

---
+++ ctrl/if.js
@@ -0,0 +1,8 @@
+var x = 15;
+if (x >= 10) {
+  document.writeln('変数xは10以上です');
+} else if (x >= 20) {
+  document.writeln('変数xは20以上です');
+} else {
+  document.writeln('変数xは10未満です');
+}
---
+++ ctrl/switch.js
@@ -0,0 +1,14 @@
+var rank = 'B';
+switch(rank){
+  case 'A':
+    document.writeln('Aランクです');
+    break;
+  case 'B':
+    document.writeln('Bランクです');
+    break;
+  case 'C':
+    document.writeln('Cランクです');
+    break;
+  default:
+    document.writeln('ランク外です');
+}
